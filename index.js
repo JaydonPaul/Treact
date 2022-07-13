bea33ac60277ecdb7f385cdd19a40e018a04b355
Jaydon <jaydonpaul2002@gmail.com>
2022-07-12 23:24:56 -0400

    Treact

---
+++ index.js
@@ -0,0 +1,48 @@
+function openList1(){
+    document.body.classList += " list--open-1"
+}
+
+function closeList1(){
+    document.body.classList.remove('list--open-1')
+}
+
+function openList2(){
+    document.body.classList += " list--open-2"
+}
+
+function closeList2(){
+    document.body.classList.remove('list--open-2')
+}
+
+function openList3(){
+    document.body.classList += " list--open-3"
+}
+
+function closeList3(){
+    document.body.classList.remove('list--open-3')
+}
+
+function openList4(){
+    document.body.classList += " list--open-4"
+}
+
+function closeList4(){
+    document.body.classList.remove('list--open-4')
+}
+
+function openList5(){
+    document.body.classList += " list--open-5"
+}
+
+function closeList5(){
+    document.body.classList.remove('list--open-5')
+}
+
+function openList6(){
+    document.body.classList += " list--open-6"
+}
+
+function closeList6(){
+    document.body.classList.remove('list--open-6')
+}
+
